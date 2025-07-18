b45b506dbe535d9d4f420110e010c6a0dcf4884f
MD. Rashidul Hasan <hamimhasan244@gmail.com>
2025-07-19 03:13:07 +0600

    Add files via upload

---
+++ OUTERN_The Bloody Escape/OUTERN_The Bloody Escape/OUTERN.h
@@ -0,0 +1,33 @@
+#ifndef OUTERN__H
+#define OUTERN__H
+
+#include "iGraphics.h"
+#include <iostream>
+#include <ctime>
+#include <thread>
+#define Default_window_width 1520
+#define Default_window_height 855
+
+namespace Game_Outern
+{
+
+class blinking_Cursour
+{
+private:
+    int blinking_Cursour_Color[3] = { 0, 0, 0 },blinking_Cursour_Show = 0;
+public:
+    void setBlinkingCursourColor(int r,int g,int b)
+    {
+
+    }
+};
+
+
+
+}
+
+
+
+
+
+#endif
